9594c0838b7e58ab2e4c2a177fcc2011fe397d7b
Mintong <tchvsps@outlook.com>
2017-06-28 10:20:02 +0800

    first file

---
+++ ex1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+print "Hello World!"
+print "Hello again"
+print "I like tpying this."
+print "This is fun"
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print '你好'
